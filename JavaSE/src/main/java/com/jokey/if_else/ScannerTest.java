4acb8b150a2df8495810ca75fcecd1584cf2814e
iamjokey <1024232950@qq.com>
2020-02-19 21:55:40 +0800

    创建Java学习笔记

---
+++ JavaSE/src/main/java/com/jokey/if_else/ScannerTest.java
@@ -0,0 +1,13 @@
+package com.jokey.if_else;
+
+/**
+ * @author Jokey Zhou
+ * @version 1.0
+ * @date 2020/2/19 21:54
+ * @description: 赛博世界并不是辽阔的荒野，数据也不全是冰冷的记录，它是亲人的笑靥，它是我们的记忆。
+ */
+public class ScannerTest {
+    public static void main(String[] args) {
+
+    }
+}
